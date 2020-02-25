fcb09cf98cc99263df8099cd396bf26af8c36c5c
Paulo Azoia <pazoia.gomes@gmail.com>
2020-02-25 09:55:21 +0000

    Added log in feature
    Click log in takes to log in page.

---
+++ spec/controllerSpec.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe( 'Controller', () => {
+  it('creates new user', () => {
+    var 
+  });
+});
---
+++ src/index.js
@@ -0,0 +1,5 @@
+let logIn = document.getElementById('log-in');
+
+logIn.addEventListener("click", evt => {
+  window.location.href = "http://127.0.0.1:5500/login.html"
+});
